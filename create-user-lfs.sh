73c087a4d62af1c9759de9ec25117f0e496d04a8
jdruk <devjosafa@gmail.com>
2019-08-09 22:51:19 -0300

    Changes to be committed: 	new file:   .gitignore 	deleted:    config-user-lfs.sh 	deleted:    constructing a tempory system.sh 	deleted:    create-user-lfs.sh 	deleted:    init-lfs.sh 	deleted:    init-user-lfs.sh

--- create-user-lfs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo groupadd lfs
-sudo useradd -s /bin/bash -g lfs -m -k /dev/null lfs
-sudo passwd lfs
-
--- init-user-lfs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# create ...
-
-# config ...
-sudo su -s /bin/bash  -c ". `pwd`/config-user-lfs.sh" - lfs
-
