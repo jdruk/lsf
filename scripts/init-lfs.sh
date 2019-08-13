672f1e9033e9792f3f2b2c2a6beccee3532b1dad
jdruk <devjosafa@gmail.com>
2019-08-13 09:22:45 -0300

    Update scripts
    Changes to be committed:
    	new file:   create-directory-base-lfs.sh
    	renamed:    chroot-lfs.sh -> enter-system-lfs.sh
    	modified:   init-lfs.sh
    	modified:   init-user-lfs.sh

--- scripts/init-lfs.sh
+++ scripts/init-lfs.sh
@@ -5,10 +5,16 @@ LFS=/mnt/lfs
 
 export LFS
 
-if [ ! -d "{$LFS}" ]; then
+if [ ! -d $LFS ]; then
 	sudo mkdir -pv $LFS 
 fi 
 
 sudo mount -v -t ext4 $PARTITION $LFS
 
+mkdir -v $LFS/tools
+ln -sv $LFS/tools /
+# unpack lfs-8.4.tar.xz in tools
 
+mkdir -v $LFS/sources
+chmod -v a+wt $LFS /sources
+cp -rv ../packages/* $LFS/sources 
--- scripts/init-user-lfs.sh
+++ scripts/init-user-lfs.sh
@@ -2,6 +2,16 @@
 
 # create ...
 
-# config ...
+# config permission 
+sudo chwon -v lfs $LFS/tools
+sudo chwon -v lfs $LFS/sources
+
+# create tempory system!
+
+
+# create basic system
+./create-directory-base-lfs.sh
+
+# config user lfs of the host system
 sudo su -s /bin/bash  -c ". `pwd`/config-user-lfs.sh" - lfs
 
